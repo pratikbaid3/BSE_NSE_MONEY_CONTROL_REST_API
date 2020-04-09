7a6cf2553432fe682e5385b121f430dbebdae82d
pratikbaid3 <pratikbaid3@gmail.com>
2020-04-09 15:54:20 +0530

    Additional features added
    1. Added menu system
    2. Added ability to get data like the ex date, record date etc

--- latest_ca_scraper.py
+++ latest_ca_scraper.py
@@ -6,20 +6,31 @@ def latest_ca_scrape():
     res = requests.get('https://www.bseindia.com/corporates/corporate_act.aspx')
     res.raise_for_status()
     page_soup = soup(res.content,features='lxml')
-    nameAndPurpose=page_soup.find_all('td',{"class":"TTRow_left"})
-    secuarityCode=page_soup.find_all('a',{"class":"tablebluelink"})
-    noOfCorporateActions=len(nameAndPurpose)
-    i=0
-    j=0
+
+    dataRows=page_soup.find_all('tr',{"class":"TTRow"})
+    dataList=[]
+    for dataRow in dataRows:
+        dataColumns=dataRow.find_all('td')
+        data=[]
+        for dataColumn in dataColumns:
+            data.append(dataColumn.text)
+        dataList.append(data)
+
     ca_array=[]
-    while (i<noOfCorporateActions):
-        coorporate_action={
-            'secuarity_code':secuarityCode[j].text,
-            'secuarity_name':nameAndPurpose[i].text,
-            'purpose':nameAndPurpose[i+1].text
+    for data in dataList:
+        corporate_action={
+            'secuarity_code':data[0],
+            'secuarity_name':data[1],
+            'ex_date':data[2],
+            'purpose':data[3],
+            'record_date':data[4],
+            'bc_start_date':data[5],
+            'bc_end_date':data[6],
+            'nd_start_date':data[7],
+            'nd_end_date':data[8],
+            'actual_payment_date':data[9]
         }
-        ca_array.append(coorporate_action)
-        i=i+2
+        ca_array.append(corporate_action)
     latest_ca_json={
         'Latest_CA':ca_array
     }
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import latest_ca_scraper
 
 def main():
-    print(latest_ca_scraper.latest_ca_scrape())
+    print("----------MENU----------")
+    print("1. Latest corporate action")
+    choice=int(input("Enter your choice: "))
+
+    if(choice==1):
+        print(latest_ca_scraper.latest_ca_scrape())
 
 if __name__ == "__main__":
     main()
