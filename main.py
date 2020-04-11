4cc205c088ccdbb24863608f5d12b88a0e13e790
pratikbaid3 <pratikbaid3@gmail.com>
2020-04-11 22:46:58 +0530

    Added script to scrape data of a perticular company

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import latest_ca_scraper
+import perticular_company_ca_scraper
 
 def main():
     print("----------MENU----------")
     print("1. Latest corporate action")
+    print("2. Corporate action of a perticulat company")
     choice=int(input("Enter your choice: "))
 
     if(choice==1):
         print(latest_ca_scraper.latest_ca_scrape())
+    elif(choice==2):
+        secuarity_code=input("Enter the secuarity code")
+        secuarity_name=input("Enter the secuarity name")
+        perticular_company_ca_scraper.companyDataScraper(secuarity_code,secuarity_name)
 
 if __name__ == "__main__":
     main()
---
+++ perticular_company_ca_scraper.py
@@ -0,0 +1,2 @@
+def companyDataScraper(secuarity_code,secuarity_name):
+    print(secuarity_code," ",secuarity_name)
