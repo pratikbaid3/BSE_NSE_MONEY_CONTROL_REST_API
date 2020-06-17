d3be495d935eaea87a74b37226a38c5ea4ae1337
pratikbaid3 <pratikbaid3@gmail.com>
2020-06-17 12:43:14 +0530

    Restarting project

--- code/bse_latest_ca_scraper.py
+++ code/bse_latest_ca_scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 
 def latest_ca_scrape():
-    res = requests.get('https://www.bseindia.com/corporates/corporate_act.aspx')
+    res = requests.get('https://www.bseindia.com/corporates/corporate_act.aspx',headers={'User-Agent':'Mozilla/5.0'})
     res.raise_for_status()
     page_soup = soup(res.content,features='lxml')
 
