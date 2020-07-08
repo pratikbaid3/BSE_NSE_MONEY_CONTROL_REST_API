6749274636b598615001a38bb591a0c778883cc7
pratikbaid3 <pratikbaid3@gmail.com>
2020-07-08 20:21:03 +0530

    Company CA get server from db

--- code/app.py
+++ code/app.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 import bse_latest_ca_scraper
 import bse_company_ca_scraper
 import bse_latest_ca
+import bse_company_ca
 
 app=Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS']=True
@@ -19,8 +20,14 @@ class LatestCA(Resource):
     def get(self):
         return{'latest_ca':bse_latest_ca.latest_ca()}
 
-#Particular company corporate action (Scraper & Server)
+#Particular company corporate action from historical data
 class CompanyCA(Resource):
+    def get(self,code):
+        return{'ca':bse_company_ca.company_ca(code)}
+
+
+#Particular company corporate action (Scraper & Server)
+class CompanyCAScraper(Resource):
     def get(self,name,code):
         return {'company_ca':bse_company_ca_scraper.company_ca_scraper(name,code)}
 
@@ -31,7 +38,8 @@ class LatestCAScraper(Resource):
         
 
 api.add_resource(LatestCA,'/api/latestca')
-api.add_resource(CompanyCA,'/api/companyca/<string:name>/<string:code>')
+api.add_resource(CompanyCA,'/api/companyca/<string:code>')
+api.add_resource(CompanyCAScraper,'/api/companycascraper/<string:name>/<string:code>')
 api.add_resource(LatestCAScraper,'/api/latestcascraper')
 
 if __name__=='__main__':
---
+++ code/bse_company_ca.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+def company_ca(security_code):
+    conn=sqlite3.connect('corporate_action.db')
+    c=conn.cursor()
+    c.execute(f'SELECT * FROM latest_bse_ca WHERE security_code = {security_code}')
+    ca_array=[]
+    for data in c:
+        corporate_action={
+            'security_code':data[1],
+            'security_name':data[2],
+            'ex_date':data[3],
+            'purpose':data[4],
+            'record_date':data[5],
+            'bc_start_date':data[6],
+            'bc_end_date':data[7],
+            'nd_start_date':data[8],
+            'nd_end_date':data[9],
+            'actual_payment_date':data[10]
+        }
+        ca_array.append(corporate_action)
+    return (ca_array)
