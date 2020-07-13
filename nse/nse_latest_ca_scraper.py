0084691742d87c62097e81bd73c545355a9a8131
pratikbaid3 <pratikbaid3@gmail.com>
2020-07-13 09:29:15 +0530

    Implemented headder for nse

--- nse/nse_latest_ca_scraper.py
+++ nse/nse_latest_ca_scraper.py
@@ -67,7 +67,7 @@ class NSEScraper:
             res = requests.get(
                 self.NSE_URL.format(typ),
                 headers={
-                    "User-Agent": "Mozilla/5.0"
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
                 },
             )
             res.raise_for_status()
