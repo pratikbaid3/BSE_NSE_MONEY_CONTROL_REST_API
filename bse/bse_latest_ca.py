0563d3b6cd76fd966f17bbbb4a4ab9fa5ed42cd6
pratikbaid3 <pratikbaid3@gmail.com>
2020-07-16 16:02:55 +0530

    Refactored output of bse corporate action api res

--- bse/bse_latest_ca.py
+++ bse/bse_latest_ca.py
@@ -6,9 +6,15 @@ def latest_ca():
     c.execute('SELECT * FROM latest_bse_ca')
     ca_array=[]
     for data in c:
+        security_name=data[2][1:len(data[2])-1]
+        if(data[10]=='\n-\n' ):
+            actual_payment_data='-'
+        else:
+            actual_payment_data=data[10]
+    
         corporate_action={
             'security_code':data[1],
-            'security_name':data[2],
+            'security_name':security_name,
             'ex_date':data[3],
             'purpose':data[4],
             'record_date':data[5],
@@ -16,7 +22,7 @@ def latest_ca():
             'bc_end_date':data[7],
             'nd_start_date':data[8],
             'nd_end_date':data[9],
-            'actual_payment_date':data[10]
+            'actual_payment_date':actual_payment_data
         }
         ca_array.append(corporate_action)
     conn.commit()
