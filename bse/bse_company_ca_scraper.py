1be6c9db5c6d3542f191de5f9dca8a5e9a7511fb
pratikbaid3 <pratikbaid3@gmail.com>
2020-07-21 17:09:38 +0530

    DB Refresh 1

--- bse/bse_company_ca_scraper.py
+++ bse/bse_company_ca_scraper.py
@@ -19,7 +19,7 @@ def company_ca_scraper(security_name,security_code):
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--incognito')
     options.add_argument("--headless")
-    driver = webdriver.Chrome("/Users/pratikbaid/Developer/chromedriver", options=options)
+    driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=options)
 
     driver.get('https://www.bseindia.com/corporates/corporate_act.aspx')
     name_xpath = '//*[@id="ContentPlaceHolder1_SmartSearch_smartSearch"]'
@@ -102,7 +102,7 @@ company_list=[]
 for comp in c_new:
     company_list.append(comp)
 list_len=len(company_list)
-for i in range(593,list_len):
+for i in range(696,list_len):
     # Adding data to the database
     company=company_list[i]
     dataList=company_ca_scraper(company[1],company[0])
@@ -122,9 +122,9 @@ for i in range(593,list_len):
                 c.execute(add_data_to_db,(uniqueKey,data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9]))
             except:
                 print('Skipped')
-    print('SLEEPING')
-    time.sleep(30)
-    print('SLEEP OVER')
+    # print('SLEEPING')
+    # time.sleep(30)
+    # print('SLEEP OVER')
     print()
     conn.commit()
 conn.close()
